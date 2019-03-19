86248587440635daa6a9d2b83c94f01f5692156b
Sergey Igoshin <sergey.igoshin@agilent.com>
2019-03-19 16:23:25 -0700

    added Modal

--- src/ModalWindow.js
+++ src/ModalWindow.js
@@ -2,17 +2,11 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
-//import 'react-bootstrap/dist/react-bootstrap.js';
-//import Modal from 'react-bootstrap/Modal';
-var ReactBootstrap = require("react-bootstrap");
+const ReactBootstrap = require("react-bootstrap");
 
-
-var Panel = ReactBootstrap.Panel, Accordion = ReactBootstrap.Accordion;
-var Button = ReactBootstrap.Button, Input = ReactBootstrap.Input;
-var ButtonToolbar = ReactBootstrap.ButtonToolbar;
+var Button = ReactBootstrap.Button
+var Input = ReactBootstrap.Input;
 var Modal = ReactBootstrap.Modal;
-var OverlayTrigger = ReactBootstrap.OverlayTrigger;
-var ListGroup = ReactBootstrap.ListGroup,ListGroupItem = ReactBootstrap.ListGroupItem;
 
 class ModalWindow extends Component {
     constructor(){
@@ -26,9 +20,21 @@ class ModalWindow extends Component {
 render() {
     return (
         <div>
-            <Button name = 'OpenModal' onClick = { () => this.props.operateModal() }/>
-            <Modal show = {this.props.showHere} onHide = {() => this.handleClose()}/>
-            </div>
+            <Button onClick = { () => this.props.operateModal() }> OpenModal </Button>
+            <Modal show = {this.props.showHere} onHide = {() => this.props.operateModal()}>
+                <Modal.Header closeButton>
+                    <Modal.Title id="modalTitle">Add new VM</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={() => console.log('Create')} bsStyle="primary" id="addButton" disabled>Add Recipe</Button>
+                    <Button onClick={() => this.props.operateModal()}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
     );
 }
 }
